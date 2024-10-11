6ca37aecac5f027739ec1a0c9bb42d795511ecc8
qkenn <github.com.sampling990@passinbox.com>
2024-10-11 15:25:19 +0530

    style(fonts): add inter font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,14 @@
+import { Inter } from 'next/font/google';
 import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );

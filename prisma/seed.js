03ecac3e9abf741db80b795b8c8d37b48db4478c
qkenn <github.com.sampling990@passinbox.com>
2024-10-13 20:27:42 +0530

    refact(prisma): optimize db seed script

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,39 +1,70 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-// data imports
 import subjects from '../src/lib/placeholder-data/subjects.json' assert { type: 'json' };
 import universities from '../src/lib/placeholder-data/universities.json' assert { type: 'json' };
 import courses from '../src/lib/placeholder-data/courses.json' assert { type: 'json' };
 import programmes from '../src/lib/placeholder-data/programmes.json' assert { type: 'json' };
 
-// create db entries utility
-async function createEntries(model, data) {
-  const created = await model.createManyAndReturn({
-    data,
-    skipDuplicates: true,
-  });
-
-  console.log(created);
-}
-
-// delete all db entries utility
-async function deleteAllEntries() {
-  await prisma.$transaction([
-    prisma.programme.deleteMany(),
-    prisma.university.deleteMany(),
-    prisma.course.deleteMany(),
-    prisma.subject.deleteMany(),
-  ]);
-}
-
 async function main() {
-  await deleteAllEntries();
+  // some models depends on other models
+  // hence, Promise.all() models seperately
 
-  await createEntries(prisma.subject, subjects);
-  await createEntries(prisma.university, universities);
-  await createEntries(prisma.course, courses);
-  await createEntries(prisma.programme, programmes);
+  await Promise.all([
+    subjects.map((subject) => {
+      return prisma.subject.upsert({
+        where: { id: subject.id },
+        update: {},
+        create: {
+          name: subject.name,
+        },
+      });
+    }),
+  ]);
+
+  await Promise.all([
+    universities.map((university) => {
+      return prisma.university.upsert({
+        where: { id: university.id },
+        update: {},
+        create: {
+          name: university.name,
+        },
+      });
+    }),
+  ]);
+
+  await Promise.all([
+    courses.map((course) => {
+      return prisma.course.upsert({
+        where: { id: course.id },
+        update: {},
+        create: {
+          courseCode: course.courseCode,
+          intake: course.intake,
+          name: course.name,
+          constraints: course.constraints || {},
+          subjectId: course.subjectId,
+        },
+      });
+    }),
+  ]);
+
+  await Promise.all([
+    programmes.map((programme) => {
+      return prisma.programme.upsert({
+        where: { id: programme.id },
+        update: {},
+        create: {
+          name: programme.name,
+          universityId: programme.universityId,
+          courseId: programme.courseId,
+          keywords: programme.keywords || [],
+          uniCode: programme.uniCode,
+        },
+      });
+    }),
+  ]);
 }
 
 main()
